0e3805a3762af64d42b0b4e571cf15965e03e6c6
Esther <eympessanha@gmail.com>
2024-02-05 09:04:03 -0300

    intro

---
+++ olaMundo.go
@@ -0,0 +1,17 @@
+/* - identado
+	- não precisa de ;
+
+	-para rodar (terminal):
+		go build .\olaMundo.go
+		.\olaMundo.exe
+	
+	-go run .\olaMundo.go    salva numa pasta temporária, não cria arquivo executável
+	-ctrl shift B para executar tasks (rodar sozinho)*/
+
+package main
+
+import "fmt"
+
+func main(){
+	fmt.Println("Olá, Mundo!")
+}
