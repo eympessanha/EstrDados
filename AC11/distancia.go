b30eae1ed380badddc96e473c07c1154353ae967
Esther Pessanha <eympessanha@gmail.com>
2024-05-08 14:06:27 -0300

    distancia.go

---
+++ AC11/distancia.go
@@ -0,0 +1,14 @@
+package main
+ 
+import "fmt"
+ 
+func main() {
+    
+    var distancia int
+    fmt.Scanln(&distancia)
+    
+    tempo := distancia * 2
+    
+    fmt.Println(tempo, "minutos")
+    
+}
